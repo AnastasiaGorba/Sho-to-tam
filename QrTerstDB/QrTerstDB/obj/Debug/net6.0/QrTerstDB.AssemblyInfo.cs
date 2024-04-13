c2e766916a30bedc13ec1a3c0c55fa608407f53b
fishxet <96199271+fishxet@users.noreply.github.com>
2024-04-13 14:51:45 +0300

    test:

--- QrTerstDB/QrTerstDB/obj/Debug/net6.0/QrTerstDB.AssemblyInfo.cs
+++ QrTerstDB/QrTerstDB/obj/Debug/net6.0/QrTerstDB.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QrTerstDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de9410e39f56f5a132445214c78248d3093f9f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f6045dbc9a4c308657f82f9f2d1a560832f9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QrTerstDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QrTerstDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
